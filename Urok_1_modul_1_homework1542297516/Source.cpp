0719a9a94687e3e58e3a3e0e07515f3d020e0dce
Александр Горячев <@local.itstep>
2018-11-27 21:10:13 +0600

    Добавьте файлы проекта.

---
+++ Urok_1_modul_1_homework1542297516/Source.cpp
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <iostream>
+#include<locale.h>
+#include<math.h>
+int main()
+{
+
+	//task5
+	/*int a, b, c, d, r, pr;
+	printf("a:  ");
+	scanf_s("%d", &a);
+
+	printf("b:  ");
+	scanf_s("%d", &b);
+	c = a + b;
+	d = a - b;
+	r = a * b;
+	pr = a / b;
+	printf("%d\n", c);
+	printf("%d\n", d);
+	printf("%d\n", r);
+	printf("%d\n", pr);*/
+
+
+
+	//task4
+	/*int a, b, c, d, r, pr;
+	printf("a:  ");
+	scanf_s("%d", &a);
+	
+	printf("b:  ");
+	scanf_s("%d", &b);
+	c = pow(a, 2) + pow(b, 2);
+	d = pow(a, 2) - pow(b, 2);
+	r = pow(a, 2) * pow(b, 2);
+	pr = pow(a, 2) / pow(b, 2);
+	printf("%d\n", c);
+	printf("%d\n", d);
+	printf("%d\n", r);
+	printf("%d\n", pr);*/
+
+	//task3
+	/*int a, b, c;
+	printf("a:  ");
+	scanf_s("%d", &a);
+	printf("b:  ");
+	scanf_s("%d", &b);
+	c = a + b;
+	printf("%d\n", c);*/
+	
+	
+	/*task2*/
+	/*int x, y, z, a, b, c, d;
+	printf("x:  ");
+	scanf_s("%d", &x);
+	
+	printf("y:  ");
+	scanf_s("%d", &y);
+	
+	printf("z:  ");
+	scanf_s("%d", &z);
+
+	a = 3;
+	b = a + 2;
+	c = b + 12;
+	d = (x*a) + (y*b) + (z*c);
+	printf("%d\n", d);*/
+
+
+	/*task1*/
+	/*int x, y, c;  
+	printf("x:  ");
+	scanf_s("%d", &x);
+	printf("y:  ");
+	scanf_s("%d", &y);
+	c = (x - y) /1 + (x*y);
+	printf("%d\n", c);*/
+
+
+
+
+
+
+
+
+
+
+	system("pause");
+}
